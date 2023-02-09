96755cc6bcb166c641c5e710eb8b450573c1af70
Brian Straub <cbstraub@protonmail.com>
2023-02-09 16:22:51 -0700

    removed _redirects

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,19 +7,19 @@ const Footer = () => {
   
     <div className='footer'>
       
-      {/* <a href='https://www.instagram.com/vanomoose/' target='_blank' rel="noreferrer noopener">
+      <a href='https://www.instagram.com/vanomoose/' target='_blank' rel="noreferrer noopener">
         <button>
           <SiInstagram className='footer__icon'/>
         </button>
-      </a> */}
+      </a>
 
         <p>© Deadwax, LLC </p>  <p>dw-0023-A-1</p>  <h6>ß</h6>
         
-       {/* <a href='https://www.linkedin.com/in/cbstraub?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BZmQvGSpcS6SxbqE6XssGRQ%3D%3D' target='_blank' rel="noreferrer noopener">
+       <a href='https://www.linkedin.com/in/cbstraub?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BZmQvGSpcS6SxbqE6XssGRQ%3D%3D' target='_blank' rel="noreferrer noopener">
         <button>
           <SiLinkedin className='footer__icon'/>
         </button>
-      </a>  */}
+      </a> 
 
     </div>
    
