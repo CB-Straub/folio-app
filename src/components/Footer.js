84a3d1de9e09bd973fa9c6978eb6e922da6cea81
Brian Straub <cbstraub@protonmail.com>
2023-02-09 12:12:46 -0700

    bad css selector fixes test 1

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
         </button>
         </a>
 
-        <p>© Deadwax, LLC </p>  <p>dw-0023-A-1</p>  <h6> ß</h6>
+        <p>© Deadwax, LLC </p>  <p>dw-0023-A-1</p>  <h6>ß</h6>
         
        <a href='https://www.linkedin.com/in/cbstraub?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BZmQvGSpcS6SxbqE6XssGRQ%3D%3D' target='_blank' rel="noreferrer noopener">
         <button>
